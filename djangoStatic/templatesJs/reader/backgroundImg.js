178abb0cb21132244592d03ffe8b7c1da3f7da94
0lidaxiang <lidaxiangregister@126.com>
2017-10-16 11:32:56 +0800

    Delete all migrations folders and djangoStatic folder.

--- djangoStatic/templatesJs/reader/backgroundImg.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  document.body.clientHeight = window.screen.height ;
-});
--- djangoStatic/templatesJs/reader/goBackPage.js
+++
@@ -1,3 +0,0 @@
-function go_back() {
-  window.history.back();
-}
